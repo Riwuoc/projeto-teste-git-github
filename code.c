319fa96c90762f680f14c98f6f4f366a13bdc28a
Riwuoc <gabtor@alunos.edu.br>
2022-06-30 00:47:06 -0300

    Generalização dos tamanhos de estrada

--- code.c
+++ code.c
@@ -1,25 +1,24 @@
 // Gabriel Torres
 #include <stdio.h>
 #include <stdlib.h>
-
+#define tam 50
 int main()
 {
-	int vetorA[20], vetorB[20], i, aux=5;
+	int vetorA[tam], vetorB[tam], i, aux=5;
 
-	for(i=0;i<20;i++){
+	for(i=0;i<tam;i++){
 		vetorA[i]=aux;
 		aux+=1;
 		printf("vetorA[%d] = %d\n",i, vetorA[i] );
 	}
 	aux=19;
-	for(i=0;i<20;i++){
+	for(i=0;i<tam;i++){
 		vetorB[aux]=vetorA[i];
 		aux--;
 	}
-
 	printf("\n");
 
-	for(i=0;i<20;i++){
+	for(i=0;i<tam;i++){
 		printf("vetorB[%d] = %d\n",i, vetorB[i] );
 	}
 }
